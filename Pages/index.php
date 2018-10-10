766550e19e4f64cd075678f18fc4e08659a9797b
PatthamapornL <42514050+PatthamapornL@users.noreply.github.com>
2018-10-11 02:00:15 +0700

    Commit
    Template and Example

---
+++ Pages/index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html>
+
+<?php include 'css.php';?>
+
+<body class="theme-red">
+    <?php include 'MasterPage.php';?>
+
+    <section class="content">
+        <div class="container-fluid">
+        </div>
+    </section>
+
+    <?php include 'js.php';?>
+</body>
+
+</html>
---
+++ Pages/js.php
@@ -0,0 +1,39 @@
+
+    <!-- Jquery Core Js -->
+    <script src="../assets/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../assets/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../assets/plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="../assets/plugins/jquery-countto/jquery.countTo.js"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="../assets/plugins/raphael/raphael.min.js"></script>
+    <script src="../assets/plugins/morrisjs/morris.js"></script>
+
+    <!-- ChartJs -->
+    <script src="../assets/plugins/chartjs/Chart.bundle.js"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="../assets/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="../assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="../assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../assets/js/admin.js"></script>
+    <script src="../assets/js/pages/tables/jquery-datatable.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../assets/js/demo.js"></script>
